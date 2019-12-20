0f5418974ee55b8ce7fa6de24b1cb1162c629088
Makcime <marlier.maxime@gmail.com>
2019-12-20 19:24:45 +0100

    merge  balena  sound project

---
+++ stib/scripts/test_gui.py
@@ -0,0 +1,7 @@
+#python3-tk
+import tkinter as tk       #Import Tkinter
+root = tk.Tk()               #Bind Tkinter to the root object
+root.attributes('-fullscreen', True)
+root.geometry("800x600")             #Set the window dimensions
+# root.bind('<KeyPress>', onKeyPress)       #For example, bind ssed
+root.mainloop()         #Starts the Tkinter and onKeyPress event
